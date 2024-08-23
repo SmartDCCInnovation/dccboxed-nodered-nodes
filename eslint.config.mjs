457466184e1da093f5bc0775111b6007d6baca5a
Karim Kanso <Karim.Kanso@smartdcc.co.uk>
2024-08-23 18:01:25 +0100

    fix: eslint v9

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import globals from 'globals'
+import prettiereslint from 'eslint-plugin-prettier/recommended'
+
+export default tseslint.config({
+  languageOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    globals: { ...globals.nodeBuiltin },
+    parserOptions: {
+      ecmaVersion: 2021,
+      project: './tsconfig.json',
+    },
+  },
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    prettiereslint,
+  ],
+  rules: {
+    eqeqeq: ['error', 'always'],
+  },
+  ignores: [
+    '**/eslint.config.mjs',
+    '**/jest.config.js',
+    '**/*.test.ts',
+    'dist/**',
+    '**/*.editor.ts',
+  ],
+})
